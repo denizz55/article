0f84c551903e12293edd4a5177f8061e4066d044
denizz55 <117968410+denizz55@users.noreply.github.com>
2024-03-13 14:02:17 +0300

    Add files via upload

---
+++ backend_prj/backend/modules/blog/views.py
@@ -0,0 +1,26 @@
+
+from django.views.generic import ListView, DetailView
+
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/articles_list.html'
+    context_object_name = 'articles'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+    
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/articles_detail.html'
+    context_object_name = 'article'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        return context
